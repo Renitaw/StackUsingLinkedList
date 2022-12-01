48af56c3f11d970a0a023794440c143b16a1694f
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2022-12-01 16:10:01 +0700

    create pop method in Stacks class

--- StackUsingLinkedList/Program.cs
+++ StackUsingLinkedList/Program.cs
@@ -41,5 +41,11 @@ namespace Stack_Using_Linked_List
             top = fresh;
             Console.WriteLine("\n" + element + " pushed. ");
         }
+
+        public void pop()
+        {
+            Console.WriteLine("\nThe popped element is: " + top.info);
+            top = top.next; //Make top point to the next node in sequence
+        }
     }
 }
