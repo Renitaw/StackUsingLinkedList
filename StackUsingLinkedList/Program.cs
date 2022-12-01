db84f892e301b49932c2169a58cf4e58456c8d28
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2022-12-01 16:15:03 +0700

    create display method in Stacks class

--- StackUsingLinkedList/Program.cs
+++ StackUsingLinkedList/Program.cs
@@ -47,5 +47,21 @@ namespace Stack_Using_Linked_List
             Console.WriteLine("\nThe popped element is: " + top.info);
             top = top.next; //Make top point to the next node in sequence
         }
+
+        public void display()
+        {
+            Node tmp;
+            if (empty())  //if stack is empty
+                Console.WriteLine("\nStack Empty");
+            else
+            {
+                //Traverse the list from begginning till end
+                for (tmp = top; tmp != null; tmp = tmp.next)
+                {
+                    Console.WriteLine(tmp.info);
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
